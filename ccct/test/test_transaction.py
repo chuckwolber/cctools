e7b911cad8db34d38646d8fbec7aac11a3754b86
Chuck Wolber <chuckwolber@gmail.com>
2025-03-21 23:30:09 -0700

    test/test_transaction.py: Native type fix
    0bfdc0324c6 reverted to native types, which broke some testing that
    relies on object types. This fixes the testing breakage.
    
    Signed-off-by: Chuck Wolber <chuckwolber@gmail.com>

--- ccct/test/test_transaction.py
+++ ccct/test/test_transaction.py
@@ -70,32 +70,32 @@ class TestTransaction(unittest.TestCase):
             self.assertTrue(tt.tm_mday == i)
 
         tl = [
-                ['8423858PG12AM3XZE', datetime.datetime(2024, 11, 1, 12, 0, tzinfo=datetime.timezone.utc), 'CREDIT', 123.45, 'PAYMENT - THANK YOU', None],
-                ['8423858PG12AM3XZE', datetime.datetime(2024, 11, 1, 12, 0, tzinfo=datetime.timezone.utc), 'CREDIT', 8675.3, 'PAYMENT - THANK YOU', None],
-                ['8423858PG12AM3XZE', datetime.datetime(2024, 11, 1, 12, 0, tzinfo=datetime.timezone.utc), 'CREDIT', 123.45, 'PAYMENT - THANK YOU', None],
-                ['8423858PG12AM3XZE', datetime.datetime(2024, 11, 1, 12, 0, tzinfo=datetime.timezone.utc), 'CREDIT', 999.99, 'PAYMENT - THANK YOU', None],
-                ['20241126000InterestChargeonPurchases', datetime.datetime(2024, 11, 1, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', 0.0, 'Interest Charge on Purchases', None],
-                ['2445106NS8WXQ9TXV', datetime.datetime(2024, 11, 2, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -1.96, 'ABCDEFG\t 123-456', 'ABCDEFG\t 800-555-1212 OR'],
-                ['20241126000Redacted1', datetime.datetime(2024, 11, 3, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', 0.0, 'Transaction 1', None],
-                ['20241126000Redacted2', datetime.datetime(2024, 11, 4, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', 0.0, 'Transaction 2', None],
-                ['Redacted3', datetime.datetime(2024, 11, 5, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -15.96, 'Transaction 3', 'Memo for Transaction 3'],
-                ['Redacted4', datetime.datetime(2024, 11, 6, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -49.6, 'Transaction 4', 'Memo for Transaction 4'],
-                ['Redacted5', datetime.datetime(2024, 11, 7, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -42.98, 'Transaction 5', 'Memo for Transaction 5'],
-                ['Redacted6', datetime.datetime(2024, 11, 8, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -55.0, 'Transaction 6', 'Memo for Transaction 6'],
-                ['Redacted7', datetime.datetime(2024, 11, 9, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -1.2, 'Transaction 7', 'Memo for Transaction 7'],
-                ['Redacted8', datetime.datetime(2024, 11, 10, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -8.23, 'Transaction 8', 'Memo for Transaction 8'],
-                ['Redacted9', datetime.datetime(2024, 11, 11, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -77.51, 'Transaction 9', 'Memo for Transaction 9'],
-                ['Redacted10', datetime.datetime(2024, 11, 12, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -35.0, 'Transaction 10', 'Memo for Transaction 10'],
-                ['Redacted11', datetime.datetime(2024, 11, 13, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -24.0, 'Transaction 11', 'Memo for Transaction 11'],
-                ['Redacted12', datetime.datetime(2024, 11, 14, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -33.05, 'Transaction 12', 'Memo for Transaction 12'],
-                ['Redacted13', datetime.datetime(2024, 11, 15, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -49.55, 'Transaction 13', 'Memo for Transaction 13'],
-                ['Redacted14', datetime.datetime(2024, 11, 16, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -16.35, 'Transaction 14', 'Memo for Transaction 14'],
-                ['Redacted15', datetime.datetime(2024, 11, 17, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -30.63, 'Transaction 15', 'Memo for Transaction 15'],
-                ['Redacted16', datetime.datetime(2024, 11, 18, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -64.47, 'Transaction 16', 'Memo for Transaction 16'],
-                ['Redacted17', datetime.datetime(2024, 11, 19, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -149.55, 'Transaction 17', 'Memo for Transaction 17'],
-                ['Redacted18', datetime.datetime(2024, 11, 20, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -126.35, 'Transaction 18', 'Memo for Transaction 18'],
-                ['Redacted19', datetime.datetime(2024, 11, 21, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -309.63, 'Transaction 19', 'Memo for Transaction 19'],
-                ['Redacted20', datetime.datetime(2024, 11, 22, 12, 0, tzinfo=datetime.timezone.utc), 'DEBIT', -964.47, 'Transaction 20', 'Memo for Transaction 20']
+                ['8423858PG12AM3XZE', '2024-11-01 12:00:00+00:00', 'CREDIT', 123.45, 'PAYMENT - THANK YOU', None],
+                ['8423858PG12AM3XZE', '2024-11-01 12:00:00+00:00', 'CREDIT', 8675.3, 'PAYMENT - THANK YOU', None],
+                ['8423858PG12AM3XZE', '2024-11-01 12:00:00+00:00', 'CREDIT', 123.45, 'PAYMENT - THANK YOU', None],
+                ['8423858PG12AM3XZE', '2024-11-01 12:00:00+00:00', 'CREDIT', 999.99, 'PAYMENT - THANK YOU', None],
+                ['20241126000InterestChargeonPurchases', '2024-11-01 12:00:00+00:00', 'DEBIT', 0.0, 'Interest Charge on Purchases', None],
+                ['2445106NS8WXQ9TXV', '2024-11-02 12:00:00+00:00', 'DEBIT', -1.96, 'ABCDEFG\t 123-456', 'ABCDEFG\t 800-555-1212 OR'],
+                ['20241126000Redacted1', '2024-11-03 12:00:00+00:00', 'DEBIT', 0.0, 'Transaction 1', None],
+                ['20241126000Redacted2', '2024-11-04 12:00:00+00:00', 'DEBIT', 0.0, 'Transaction 2', None],
+                ['Redacted3', '2024-11-05 12:00:00+00:00', 'DEBIT', -15.96, 'Transaction 3', 'Memo for Transaction 3'],
+                ['Redacted4', '2024-11-06 12:00:00+00:00', 'DEBIT', -49.6, 'Transaction 4', 'Memo for Transaction 4'],
+                ['Redacted5', '2024-11-07 12:00:00+00:00', 'DEBIT', -42.98, 'Transaction 5', 'Memo for Transaction 5'],
+                ['Redacted6', '2024-11-08 12:00:00+00:00', 'DEBIT', -55.0, 'Transaction 6', 'Memo for Transaction 6'],
+                ['Redacted7', '2024-11-09 12:00:00+00:00', 'DEBIT', -1.2, 'Transaction 7', 'Memo for Transaction 7'],
+                ['Redacted8','2024-11-10 12:00:00+00:00', 'DEBIT', -8.23, 'Transaction 8', 'Memo for Transaction 8'],
+                ['Redacted9', '2024-11-11 12:00:00+00:00', 'DEBIT', -77.51, 'Transaction 9', 'Memo for Transaction 9'],
+                ['Redacted10', '2024-11-12 12:00:00+00:00', 'DEBIT', -35.0, 'Transaction 10', 'Memo for Transaction 10'],
+                ['Redacted11', '2024-11-13 12:00:00+00:00', 'DEBIT', -24.0, 'Transaction 11', 'Memo for Transaction 11'],
+                ['Redacted12', '2024-11-14 12:00:00+00:00', 'DEBIT', -33.05, 'Transaction 12', 'Memo for Transaction 12'],
+                ['Redacted13', '2024-11-15 12:00:00+00:00', 'DEBIT', -49.55, 'Transaction 13', 'Memo for Transaction 13'],
+                ['Redacted14', '2024-11-16 12:00:00+00:00', 'DEBIT', -16.35, 'Transaction 14', 'Memo for Transaction 14'],
+                ['Redacted15', '2024-11-17 12:00:00+00:00', 'DEBIT', -30.63, 'Transaction 15', 'Memo for Transaction 15'],
+                ['Redacted16', '2024-11-18 12:00:00+00:00', 'DEBIT', -64.47, 'Transaction 16', 'Memo for Transaction 16'],
+                ['Redacted17', '2024-11-19 12:00:00+00:00', 'DEBIT', -149.55, 'Transaction 17', 'Memo for Transaction 17'],
+                ['Redacted18', '2024-11-20 12:00:00+00:00', 'DEBIT', -126.35, 'Transaction 18', 'Memo for Transaction 18'],
+                ['Redacted19', '2024-11-21 12:00:00+00:00', 'DEBIT', -309.63, 'Transaction 19', 'Memo for Transaction 19'],
+                ['Redacted20', '2024-11-22 12:00:00+00:00', 'DEBIT', -964.47, 'Transaction 20', 'Memo for Transaction 20']
         ]
         for i in range(0,26):
             self.assertEqual(ccct._transactions[i].to_list(), tl[i])
@@ -169,7 +169,7 @@ class TestTransaction(unittest.TestCase):
                    trntype="DEBIT", trnamt=-1.11, name="NAME", memo=None)
         tr = ccct.Transaction(t)
         self.assertTrue(tr.to_list()[0] == "8423858PG12AM3XZE")
-        self.assertTrue(tr.to_list()[1] == datetime.datetime(2024, 11, 5, 12, 0, tzinfo=datetime.timezone.utc))
+        self.assertTrue(tr.to_list()[1] == '2024-11-05 12:00:00+00:00')
         self.assertTrue(tr.to_list()[2] == "DEBIT")
         self.assertTrue(tr.to_list()[3] == -1.11)
         self.assertTrue(tr.to_list()[4] == "NAME")
